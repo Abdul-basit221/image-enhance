dbf3968d56ac7958d2e5aed58526ff47ea23d4d0
Abdul Basit <abbdulbasit865@gmail.com>
2026-03-03 15:32:00 +0100

    Add files via upload

--- gamma.py
+++ gamma.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 img = cv2.imread("straw.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-
+# Normalize image to 0–1
 img_norm = img / 255.0
 
 gamma = 2 # <1 brighter, >1 darker
